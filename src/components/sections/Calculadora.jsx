b1e7d6e3e2115078d45f65289d4a8e223f7c39f3
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-11-03 11:53:09 -0300

    fix: Reorganiza as secoes

--- src/components/sections/Calculadora.jsx
+++ src/components/sections/Calculadora.jsx
@@ -14,12 +14,12 @@ export default function Calculadora({ colorMode }) {
   const bgClasses = {
     dark: "bg-bgSectionOpacityDark",
     light: "bg-bgSectionOpacityLight",
-    default: "squares",
+    default: "bg-bgSectionDark",
   };
   const textClasses = {
     dark: "text-white",
     light: "text-black",
-    default: "text-black",
+    default: "text-white",
   };
   const bgClass = bgClasses[colorMode] || bgClasses.default;
   const titleColor = textClasses[colorMode] || textClasses.default;
@@ -27,19 +27,19 @@ export default function Calculadora({ colorMode }) {
   return (
     <>
       <SectionArea id="Calculadora" className={`${bgClass}`}>
-        <SectionHeader
-          colorMode="dark"
-          className="text-center"
-          miniTitle={content.texts.calc.miniTag}
-          sectionHeaderTitle={content.texts.calc.title}
-          sectionHeaderSubtitle={content.texts.calc.subtitle}
-          color=""
-          titleColorSet={titleColor}
-          subtitleColorSet={titleColor}
-          miniTitleBgColor="bg-minititle"
-          miniTitleTextColor="text-darker"
-        />
         <SectionWrapper className="flex justify-center">
+          <SectionHeader
+            colorMode="dark"
+            className="text-center"
+            miniTitle={content.texts.calc.miniTag}
+            sectionHeaderTitle={content.texts.calc.title}
+            sectionHeaderSubtitle={content.texts.calc.subtitle}
+            color=""
+            titleColorSet={titleColor}
+            subtitleColorSet={titleColor}
+            miniTitleBgColor="bg-minititle"
+            miniTitleTextColor="text-darker"
+          />
           <MotionDivDownToUp>
             <Button
               label={content.texts.calc.labelButton}
